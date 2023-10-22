30bd97ffc2403d1fa3c518e294559c7398488eac
BT <kornkanok_kanchana@cmu.ac.th>
2023-10-22 15:59:12 +0700

    default

--- src/main/java/com/example/pjcompo/PjCompoApplication.java
+++ src/main/java/com/example/pjcompo/PjCompoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PjCompoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PjCompoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PjCompoApplication.class, args);
+    }
 
 }
