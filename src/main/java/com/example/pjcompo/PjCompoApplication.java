ba8da6b3f73a6e3df6793f45c28fa5d39713ae32
BT <kornkanok_kanchana@cmu.ac.th>
2023-10-22 15:57:58 +0700

    first commit

---
+++ src/main/java/com/example/pjcompo/PjCompoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pjcompo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjCompoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjCompoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pjcompo/PjCompoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pjcompo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjCompoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
