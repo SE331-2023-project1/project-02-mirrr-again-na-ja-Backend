8a71c653f834fbd2b657dc98bc7f2287a285ebba
JulaDev <jokekajik2101@gmail.com>
2023-10-24 01:48:16 +0700

    update AdvisorApp and InitApp

--- src/main/java/com/example/pjcompo/config/AdvisorApp.java
+++ src/main/java/com/example/pjcompo/config/AdvisorApp.java
@@ -13,24 +13,50 @@ public class AdvisorApp implements ApplicationListener<ApplicationReadyEvent> {
     final AdvisorRepository advisorRepository;
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        //1
         advisorRepository.save(Advisor.builder()
                 .name("Nemuri")
                 .surname("Kayama")
-                .image("https://static.wikia.nocookie.net/bokunoheroacademia/images/1/1e/Midnight_Anime_Portrait.png/revision/latest/scale-to-width-down/135?cb=20200401121928")
+                .image("https://i.pinimg.com/originals/44/c9/c7/44c9c71162642ace8e3fe4bd79e9b2f9.jpg")
                 .build()
         );
+        //2
         advisorRepository.save(Advisor.builder()
                 .name("Yu")
                 .surname("Takeyama")
-                .image("https://static.wikia.nocookie.net/bokunoheroacademia/images/d/db/Mt._Lady_Anime_Portrait.png/revision/latest/scale-to-width-down/135?cb=20200321204446")
+                .image("https://i.pinimg.com/originals/18/a5/fa/18a5facbae3db807b103146d784a5d71.jpg")
                 .build()
         );
+        //3
         advisorRepository.save(Advisor.builder()
                 .name("Sorahiko")
                 .surname("Torino")
-                .image("https://static.wikia.nocookie.net/bokunoheroacademia/images/7/77/Gran_Torino_Anime_Portrait.png/revision/latest/scale-to-width-down/135?cb=20180503181559")
+                .image("https://i.pinimg.com/originals/12/46/aa/1246aa6a3e6f4c5a12f43257ecd227e2.jpg")
                 .build()
         );
+        //4
+        advisorRepository.save(Advisor.builder()
+                .name("Enji")
+                .surname("Todoroki")
+                .image("https://i.pinimg.com/originals/9b/62/6f/9b626f08cdfd242fcaa9b34470ad323f.jpg")
+                .build()
+        );
+        //5
+        advisorRepository.save(Advisor.builder()
+                .name("Anan")
+                .surname("Kurose")
+                .image("https://i.pinimg.com/originals/b0/fe/28/b0fe28767e7a68d22eeaa38b9e287b20.jpg")
+                .build()
+        );
+        //6
+        advisorRepository.save(Advisor.builder()
+                .name("Hizashi")
+                .surname("Yamada")
+                .image("https://i.pinimg.com/originals/4e/8f/22/4e8f22d17f6dfd0eac6dbdf822eb4d9c.jpg")
+                .build()
+        );
+
+
     }
 
 
--- src/main/java/com/example/pjcompo/config/InitApp.java
+++ src/main/java/com/example/pjcompo/config/InitApp.java
@@ -18,7 +18,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .name("Yosetsu")
                 .surname("Awase")
                 .course("Support combat")
-                .image("https://static.wikia.nocookie.net/bokunoheroacademia/images/e/e6/Yosetsu_Awase_Portrait.png/revision/latest/scale-to-width-down/135?cb=20181125070427")
+                .image("https://i.pinimg.com/originals/a2/3d/17/a23d17cae4eb6a8e402fd01edf61057b.jpg")
                 .build()
         );
         //2
@@ -26,7 +26,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .name("Sen")
                 .surname("Kaibara")
                 .course("Melee combat")
-                .image("https://static.wikia.nocookie.net/bokunoheroacademia/images/b/b2/Sen_Kaibara_Portrait.png/revision/latest/scale-to-width-down/135?cb=20181125070109")
+                .image("https://i.pinimg.com/originals/ed/87/82/ed8782d0d441f2d02bbe1430c720a521.jpg")
                 .build()
         );
         //3
@@ -34,7 +34,63 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 .name("Togaru")
                 .surname("Kamakiri")
                 .course("Close Range Combat")
-                .image("https://static.wikia.nocookie.net/bokunoheroacademia/images/1/1a/Togaru_Kamakiri_Portrait.png/revision/latest/scale-to-width-down/135?cb=20181125070743")
+                .image("https://i.pinimg.com/originals/6d/d7/0b/6dd70b4ae470dc34b9291f837cb50e48.jpg")
+                .build()
+        );
+        //4
+        studentRepository.save(Student.builder()
+                .name("Shihai")
+                .surname("Kuroiro")
+                .course("Melee,support combat")
+                .image("https://i.pinimg.com/originals/0d/e7/1b/0de71be81f68d881b2b5310d449e0c5f.jpg")
+                .build()
+        );
+        //5
+        studentRepository.save(Student.builder()
+                .name("Itsuka")
+                .surname("Kendo")
+                .course("Melee combat")
+                .image("https://i.pinimg.com/originals/04/39/de/0439dee136c54893daaf3c22e2a4cf4c.jpg")
+                .build()
+        );
+        //6
+        studentRepository.save(Student.builder()
+                .name("Yui")
+                .surname("Kodai")
+                .course("Long Ranged Combat")
+                .image("https://i.pinimg.com/originals/4c/0d/71/4c0d718a468cf418e64534e4554eb138.png")
+                .build()
+        );
+        //7
+        studentRepository.save(Student.builder()
+                .name("Kinoko")
+                .surname("Komori")
+                .course("Long Ranged Support")
+                .image("https://i.pinimg.com/originals/f8/be/18/f8be186008ede1e1cff00c5da2df967b.jpg")
+                .build()
+        );
+        //8
+        studentRepository.save(Student.builder()
+                .name("Mirio")
+                .surname("Togata")
+                .course("Melee combat")
+                .image("https://i.pinimg.com/originals/6d/e4/07/6de407fd96ef24d458b614bd2bbbc9ae.jpg")
+                .build()
+        );
+        //9
+        studentRepository.save(Student.builder()
+                .name("Hitoshi")
+                .surname("Shinso")
+                .course("Supportcombat,Mind control,Binding Cloth")
+                .image("https://i.pinimg.com/originals/40/67/48/4067485792a111ce107be8675e5c0a4f.jpg")
+                .build()
+        );
+        //10
+        studentRepository.save(Student.builder()
+                .name("Tamaki")
+                .surname("Amajiki")
+                .course("Melee,support combat")
+                .image("https://i.pinimg.com/originals/7a/b8/49/7ab8499c64aedb4aba26081ba1ff26e8.jpg")
                 .build()
         );
     }
